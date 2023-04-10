cc8752095833fe17ba4c091db48371117a8d4367
tamaritamari <shuhei.tamari@cureapp.jp>
2023-04-10 12:48:29 +0900

    間違えた

--- dist/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const zebrafish_1 = require("./core/zebrafish");
-const server_1 = require("./plugins/server");
-const entryPoint = `${__dirname}/../examples/serve.js`;
-const zebrafish = new zebrafish_1.Zebrafish(entryPoint, `${__dirname}/../examples`, undefined, [server_1.serverPlugin]);
-zebrafish.start();
